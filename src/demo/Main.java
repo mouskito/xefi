03f22c8d2e24fcb9bacae7a4c57b40063bf1dbc7
Moussa CAMARA <msa.camara@gmail.com>
2024-01-08 17:08:50 +0100

    correction

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,68 +1,57 @@
 package demo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-/*
- * @author: "MOussa 
- * */
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class Main {
-/*
- * CIem
- */
 	public static void main(String[] args) {
-		System.out.println("EXERCICE 1: Annee Bissextile");
+		System.out.println("Choisissez un mot.");
+		Scanner scanner = new Scanner(System.in);
+		String choix = scanner.next();
+
+		System.out.println("Voici ton choix: " +choix);
 		
-		int annee = 2033;
-		
-		if ((annee%4==0 && annee %100 != 0) || annee %400==0) {
-			System.out.println("\t L'année " +annee +" est une année bissextile");
+		//mot_inverse("Moussa");
+		if (choix.equalsIgnoreCase(mot_inverse(choix))) {
+			System.out.println(choix +" est un palindrome");
 		} else {
-			System.out.println("\t L'année " +annee +" n'est pas une année bissextile");
+			System.err.println(choix + " n'est pas un palindrome");
 		}
+		//Ternaire
+		System.out.println(choix.equalsIgnoreCase(mot_inverse(choix)) ? 
+				choix +" est un palindrome"
+				: 
+					choix +" n'est pas un palindrome"	
+				);
 		
-		System.out.println("EXERCICE 2: Tableau semaine");
-		System.out.println("Version 1 : tableau classique");
-		String [] semaine = {"Lundi","Mar","Mercredi","Jeudi","Vendredi","Samedi","Dimanc"};
-		// 1- Retirer la dernière valeur du tableau
-		String [] nouvelle = new String [semaine.length-1];
+		//OPTION2: STringBuilder
+		System.out.println("StringBulder: Choisissez un mot.");
+		Scanner scanner1 = new Scanner(System.in);
+		String choix1 = scanner1.next();
+	
+		StringBuilder builder = new StringBuilder();
+		builder.append(choix1);
+		builder = builder.reverse();
 		
-		System.out.println(Arrays.toString(semaine));
-		System.out.println(nouvelle);
+		String mot = builder.toString();
 		
-		System.out.println("Version 2: collection : ArrayList");
-		ArrayList<String> semaine1 = new ArrayList<>();
-		semaine1.add("Lundi");
-		semaine1.add("Mar");
-		semaine1.add("Mercredi");
-		semaine1.add("Jeudi");
+		String mot2 = new StringBuilder(choix1).reverse().toString();
 		
-		ArrayList<String> semaine2 = new ArrayList<>(
-				Arrays.asList(
-						"Lundi","Mar","Mercredi","Jeudi","Vendredi",
-						"Samedi", "Dimanc"));
-		// 1.	Retirer la dernière valeur du tableau
-		semaine2.remove(semaine2.size()-1);
-		
-		// 2.	Afficher les valeurs du tableau
-		System.out.println(semaine2);
-		
-		// 3.	Ajouter  la valeur ‘dimanche’ à la fin du tableau
-		semaine2.add("Dimanche");
-		System.out.println(semaine2);
-		// 4.	Remplacer le mar par mardi
-		//semaine2.set(1, "Mardi");
-		System.out.println(semaine2.indexOf("Mardi"));
-		semaine2.set(semaine2.indexOf("Mar"), "Mardi");
-		// 5.	Afficher le nombre de valeurs du tableau
-		System.out.println(semaine2.size());
-		
-		// 6.	Afficher la 5éme valeur
-		System.out.println(semaine2.get(4));
-		
-		
-		System.out.println(semaine2);
+		if (choix1.equalsIgnoreCase(mot2)) {
+			System.out.println("est un palindrome");
+		} else {
+			System.out.println("n'est pas un palindrome");
+		}
 	}
+	
+	public static String mot_inverse(String chaine) {
+		 String inverse = "";
+		for (int i = 0; i < chaine.length(); i++) {
+			inverse += chaine.charAt(chaine.length()-i -1);
+		}
+		return inverse;
+	}
+	
+
 	
 }
